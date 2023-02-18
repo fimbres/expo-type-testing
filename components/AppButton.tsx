8f8b62e47a44684740848f8d843f0fb5401ddd63
Isaac <fimbresisaac23@gmail.com>
2023-02-18 11:51:21 -0800

    new AppButton component

---
+++ components/AppButton.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react'
+import { Pressable, StyleSheet, Text } from 'react-native'
+import { Colors, Styles } from '../constants';
+
+interface AppButtonProps {
+    title: string;
+    size?: "md" | "lg";
+    onPress: () => void;
+}
+
+export const AppButton: FC<AppButtonProps> = ({ title, size = "md", onPress }) => {
+  return (
+    <Pressable style={({ pressed }) => [styles.container, styles[size], pressed && styles.pressed]} onPress={onPress}>
+        <Text style={[size === "md" ? Styles.textTitleExtraSmall : Styles.textTitleRegular, { color: Colors.white }]}>{title}</Text>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        borderRadius: 10,
+        backgroundColor: Colors.blue,
+    },
+    md: {
+        paddingVertical: 17,
+    },
+    lg: {
+        paddingVertical: 14,
+    },
+    pressed: {
+        opacity: 0.8,
+    }
+});
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export * from "./AppButton";
 export * from "./PointsBadge";
 export * from "./SafeAreaContainer";
 export * from "./ScreenHeader";
