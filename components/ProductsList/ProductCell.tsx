df5f8d87bf91494d24dc49a998c88bd0563a5de0
Isaac <fimbresisaac23@gmail.com>
2023-02-18 01:42:13 -0800

    new ProductsList component

---
+++ components/ProductsList/ProductCell.tsx
@@ -0,0 +1,69 @@
+import React, { FC } from 'react'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import { Entypo } from '@expo/vector-icons';
+import moment from 'moment';
+import 'moment/locale/es';
+
+import { IProduct } from '../../stores/types'
+import { Colors, Styles } from '../../constants';
+
+interface ProductCellProps {
+    product: IProduct;
+    onPress: () => void;
+}
+
+export const ProductCell: FC<ProductCellProps> = ({ product, onPress }) => {
+    const { image, product: name, createdAt, points, is_redemption } = product;
+
+    const getDateLabel = () => {
+        const momentDate = moment(createdAt);
+        return `${momentDate.format("d")} de ${momentDate.format("MMMM, YYYY")}`
+    }
+
+    return (
+        <Pressable style={styles.cell} onPress={onPress}>
+            <View style={styles.container}>
+                <Image source={{ uri: image }} style={styles.image}/>
+                <View>
+                    <Text style={styles.primaryText}>{name}</Text>
+                    <Text style={Styles.textCaptionExtraSmall}>{getDateLabel()}</Text>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.pointsText}>
+                    <Text style={{ color: is_redemption ? Colors.red : Colors.green }}>{is_redemption ? "-" : "+"}</Text>
+                    {points}
+                </Text>
+                <Entypo name="triangle-right" size={14} color="black" />
+            </View>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    cell: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 8,
+        paddingHorizontal: 10,
+    },
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        width: 55,
+        height: 55,
+        borderRadius: 10,
+        marginRight: 11,
+    },
+    primaryText: {
+        ...Styles.textTitleSmall,
+        marginBottom: 7,
+    },
+    pointsText: {
+        ...Styles.textTitleRegular,
+        marginRight: 10
+    }
+})
---
+++ components/ProductsList/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+import { FlatList, StyleSheet , View} from 'react-native'
+import { IProduct } from '../../stores/types'
+import { ProductCell } from './ProductCell';
+import { Colors } from '../../constants';
+
+interface ProductsListProps {
+    products: IProduct[];
+    onPress: (id: number) => void;
+}
+
+export const ProductsList: FC<ProductsListProps> = ({ products, onPress }) => {
+  return (
+    <FlatList
+      style={styles.list}
+      showsVerticalScrollIndicator={false}
+      data={products}
+      ItemSeparatorComponent={() => <View style={{ marginVertical: 4 }} />}
+      renderItem={({ item, index }) => <ProductCell key={index} product={item} onPress={() => onPress(item.id)} />}
+      ListFooterComponent={() => <View style={{ marginTop: 33 }}/>}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: Colors.white,
+    borderRadius: 16,
+    paddingVertical: 23,
+  }
+})
