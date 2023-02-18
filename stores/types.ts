8008b0e662769530df7dddab5a4772bedc4ff821
Isaac <fimbresisaac23@gmail.com>
2023-02-17 23:46:07 -0800

    new Product interface

---
+++ stores/types.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+    id: number,
+    product: string,
+    image: string,
+    points: number,
+    is_redemption: boolean,
+    createdAt: string,
+    amount?: number,
+    from_account_id?: number,
+    to_account_id?: number;
+    reason?: string,
+    verification_code?: string,
+}
