a9b4c857cd24aecdbf1da455be65c4d046211bc6
Isaac <fimbresisaac23@gmail.com>
2023-02-18 15:19:08 -0800

    new components

---
+++ components/InformationLabel.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { StyleSheet, Text, TextStyle } from 'react-native';
+
+import { Colors, Styles } from '../constants';
+
+interface InformationLabelProps {
+    primaryText: string;
+    secondaryText: string;
+    primaryTextStyle: TextStyle;
+    secondaryTextStyle: TextStyle;
+}
+
+export const InformationLabel: FC<InformationLabelProps> = ({ primaryText, secondaryText, primaryTextStyle, secondaryTextStyle }) => {
+  return (
+    <>
+        <Text style={[styles.subtitle, primaryTextStyle]}>{primaryText}</Text>
+        <Text style={secondaryTextStyle}>{secondaryText}</Text>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+    subtitle: {
+        ...Styles.textTitleSmall,
+        color: Colors.gray,
+    },
+});
