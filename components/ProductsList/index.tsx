79219a4ef106ab3b7c1be395959ab2f7d05bfe57
Isaac <fimbresisaac23@gmail.com>
2023-02-18 15:53:57 -0800

    Adding EmptyState in ProductsList

--- components/ProductsList/index.tsx
+++ components/ProductsList/index.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, Text, Image } from 'react-native';
 
 import { IProduct } from '../../types';
 import { ProductCell } from './ProductCell';
-import { Colors } from '../../constants';
+import { Colors, Styles } from '../../constants';
+import EmptyInvitesImg from "../../assets/images/empty-products.png";
 
 interface ProductsListProps {
     products: IProduct[];
@@ -19,6 +20,13 @@ export const ProductsList: FC<ProductsListProps> = ({ products, onPress }) => {
       ItemSeparatorComponent={() => <View style={{ marginVertical: 4 }} />}
       renderItem={({ item, index }) => <ProductCell key={index} product={item} onPress={() => onPress(item.id)} />}
       ListFooterComponent={() => <View style={{ marginTop: 33 }}/>}
+      ListEmptyComponent={() => (
+        <View style={styles.emptyState}>
+          <Image source={EmptyInvitesImg} style={styles.image} />
+          <Text style={styles.primaryText}>No hay movimientos registrados</Text>
+          <Text style={styles.secondaryText}>Realiza una compra y vuelve más tarde</Text>
+        </View>
+      )}
     />
   )
 }
@@ -30,5 +38,23 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     borderRadius: 16,
     paddingVertical: 23,
-  }
+  },
+  emptyState: {
+    marginTop: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 190,
+    height: 150,
+    resizeMode: "cover",
+    marginBottom: 24,
+  },
+  primaryText: {
+    ...Styles.textTitleRegular,
+  },
+  secondaryText: {
+    ...Styles.textCaptionSmall,
+    color: Colors.gray,
+  },
 })
