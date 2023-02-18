6a510a8ac58465a22f11300584589b99378e3ccf
Isaac <fimbresisaac23@gmail.com>
2023-02-18 01:42:52 -0800

    implementing HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,24 +1,33 @@
 import React, { FC } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-
-import { Styles } from '../constants'
-import { RootStackParamList } from '../navigation/types'
-import { selectAllProducts, selectNotRedemptionProducts, selectRedemptionProducts } from '../stores/productsSlice'
 import { useSelector } from 'react-redux'
-import { useGetProductsQuery } from '../stores/apiSlice'
 
-const HomeScreen: FC<NativeStackScreenProps<RootStackParamList, "Home">> = () => {
+import { Colors, Styles } from '../constants'
+import { PointsBadge, ProductsList, SafeAreaContainer, ScreenHeader } from '../components'
+import { RootStackParamList } from '../navigation/types'
+import { useGetProductsQuery } from '../stores/apiSlice'
+import { selectTotalPoints, selectAllProducts, selectNotRedemptionProducts, selectRedemptionProducts } from '../stores/productsSlice'
+
+const HomeScreen: FC<NativeStackScreenProps<RootStackParamList, "Home">> = ({ navigation }) => {
   const { isError, isLoading, error } = useGetProductsQuery({});
+  const totalPoints = useSelector(selectTotalPoints);
   const allProducts = useSelector(selectAllProducts);
   const redemptionProducts = useSelector(selectRedemptionProducts);
   const notRedemptionProducts = useSelector(selectNotRedemptionProducts);
 
   return (
-    <View style={styles.container}>
-      <Text style={Styles.textTitleGiant}>HomeScreen</Text>
-      <Text style={Styles.textCaptionExtraSmall}>HomeScreen</Text>
-    </View>
+    <SafeAreaContainer>
+      <>
+        <ScreenHeader title='Bienvenido de vuelta!' subtitle='Ruben Rodriguez' />
+        <View style={styles.container}>
+          <Text style={styles.subTitle}>Tus Puntos</Text>
+          <PointsBadge points={totalPoints}/>
+          <Text style={styles.subTitle}>Tus Movimientos</Text>
+          <ProductsList products={allProducts} onPress={(productId) => navigation.navigate("Product", { productId })} />
+        </View>
+      </>
+    </SafeAreaContainer>
   )
 }
 
@@ -26,8 +35,13 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+      flex: 1,
+      paddingHorizontal: 20,
+    },
+    subTitle: {
+      ...Styles.textTitleSmall,
+      color: Colors.gray,
+      textTransform: 'uppercase',
+      marginVertical: 20,
     }
 });
