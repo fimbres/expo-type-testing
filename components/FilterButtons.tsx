3deb3ac6845357f65ebe868c474a01efebb8b90d
Isaac <fimbresisaac23@gmail.com>
2023-02-18 15:42:47 -0800

    new components

---
+++ components/FilterButtons.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+import { View } from 'react-native'
+
+import { Filter } from '../types';
+import { AppButton } from './AppButton'
+
+interface FilterButtonsProps {
+    filter: Filter;
+    setFilter: (filter: Filter) => void;
+}
+
+export const FilterButtons: FC<FilterButtonsProps> = ({ filter, setFilter }) => {
+  return filter === "all" ?
+    (
+        <>
+            <AppButton title="Ganados" onPress={() => setFilter("no-redemption")} />
+            <View style={{ marginHorizontal: 6 }} />
+            <AppButton title="Canjeados" onPress={() => setFilter("redemption")} />
+        </>
+    ) : (
+        <AppButton title="Todos" size='lg' onPress={() => setFilter("all")} />
+    )
+}
--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,11 @@
 export * from "./AppButton";
-export * from "./ButtonsContainer";
+export * from "./FilterButtons";
 export * from "./InformationLabel";
 export * from "./Loader";
 export * from "./PointsBadge";
 export * from "./ProductImageBadge";
 export * from "./ProductsList";
 export * from "./SafeAreaContainer";
+export * from "./ScreenFooter";
 export * from "./ScreenHeader";
  
