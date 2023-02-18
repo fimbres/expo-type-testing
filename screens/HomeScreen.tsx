b6dd14ae3266b27a6dcbb177a79c9d5645d504d2
Isaac <fimbresisaac23@gmail.com>
2023-02-17 23:51:03 -0800

    implementing data filtering

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,8 +4,16 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 import { Styles } from '../constants'
 import { RootStackParamList } from '../navigation/types'
+import { selectAllProducts, selectNotRedemptionProducts, selectRedemptionProducts } from '../stores/productsSlice'
+import { useSelector } from 'react-redux'
+import { useGetProductsQuery } from '../stores/apiSlice'
 
 const HomeScreen: FC<NativeStackScreenProps<RootStackParamList, "Home">> = () => {
+  const { isError, isLoading, error } = useGetProductsQuery({});
+  const allProducts = useSelector(selectAllProducts);
+  const redemptionProducts = useSelector(selectRedemptionProducts);
+  const notRedemptionProducts = useSelector(selectNotRedemptionProducts);
+
   return (
     <View style={styles.container}>
       <Text style={Styles.textTitleGiant}>HomeScreen</Text>
