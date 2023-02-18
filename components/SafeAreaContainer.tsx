d4235bdc4f967b865093e330132eef3a1ba21d66
Isaac <fimbresisaac23@gmail.com>
2023-02-18 01:41:41 -0800

    new SafeAreaContainer component

---
+++ components/SafeAreaContainer.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactNode } from "react";
+import { SafeAreaView, StyleSheet, StyleProp, ViewStyle, Platform } from "react-native";
+import Constants from "expo-constants";
+
+import { Colors } from "../constants";
+
+interface SafeAreaContainerProps {
+  style?: StyleProp<ViewStyle>;
+  children?: ReactNode;
+}
+
+export const SafeAreaContainer: FC<SafeAreaContainerProps> = ({ style, children }) => {
+  return (
+    <SafeAreaView
+      style={[styles.container, style]}
+    >
+      {children}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.bone,
+    paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+  },
+});
