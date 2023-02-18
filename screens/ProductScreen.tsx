bbed698fb6bc31ba67477bf9077e53477648eb27
Isaac <fimbresisaac23@gmail.com>
2023-02-18 15:19:45 -0800

    Refactor code

--- screens/ProductScreen.tsx
+++ screens/ProductScreen.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react'
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { useSelector } from 'react-redux'
 
 import { Colors, Styles } from '../constants'
 import { IProduct, RootState } from '../types'
 import { RootStackParamList } from '../navigation/types'
-import { AppButton, SafeAreaContainer, ScreenHeader } from '../components'
+import { AppButton, ProductImageBadge, SafeAreaContainer, ScreenHeader, InformationLabel } from '../components'
 import { selectProductById } from '../slices/productsSlice'
 import { getDateLabel, getFormattedPoints } from '../utils/formats';
 
@@ -23,21 +23,27 @@ const ProductScreen: FC<NativeStackScreenProps<RootStackParamList, "Product">> =
         <>
             <SafeAreaContainer style={styles.statusBar} />
             <SafeAreaContainer style={{ paddingTop: 0 }}>
-            <>
-                <ScreenHeader title={name} style={styles.header} />
-                <ScrollView style={styles.container}>
-                    <View style={styles.imageContainer}>
-                        <Image source={{ uri: image }} style={styles.image} />
+                <>
+                    <ScreenHeader title={name} style={styles.header} />
+                    <ScrollView style={styles.container}>
+                        <ProductImageBadge productImageUri={image} />
+                        <InformationLabel
+                            primaryText="Detalles del producto:"
+                            primaryTextStyle={{ marginTop: 32, marginBottom: 19 }}
+                            secondaryText={`Comprado el ${getDateLabel(createdAt)}`}
+                            secondaryTextStyle={Styles.textTitleRegular}
+                        />
+                        <InformationLabel
+                            primaryText="Con esta compra acumulaste:"
+                            primaryTextStyle={{ marginTop: 20, marginBottom: 32 }}
+                            secondaryText={`${getFormattedPoints(points)} puntos`}
+                            secondaryTextStyle={Styles.textTitleExtraLarge}
+                        />
+                    </ScrollView>
+                    <View style={styles.buttonContainer}>
+                        <AppButton title='Aceptar' size='lg' onPress={() => navigation.goBack()} />
                     </View>
-                    <Text style={[styles.subtitle, { marginTop: 32, marginBottom: 19}]}>Detalles del producto:</Text>
-                    <Text style={Styles.textTitleRegular}>Comprado el {getDateLabel(createdAt)}</Text>
-                    <Text style={[styles.subtitle, { marginTop: 20, marginBottom: 32}]}>Con esta compra acumulaste:</Text>
-                    <Text style={Styles.textTitleExtraLarge}>{getFormattedPoints(points)} puntos</Text>
-                </ScrollView>
-                <View style={styles.buttonContainer}>
-                    <AppButton title='Aceptar' size='lg' onPress={() => navigation.goBack()} />
-                </View>
-            </>
+                </>
             </SafeAreaContainer>
         </>
     )
@@ -58,31 +64,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 20,
-    },
-    imageContainer: {
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        aspectRatio: 1,
-        borderRadius: 10,
-        marginTop: 19,
-        backgroundColor: Colors.white,
-        shadowColor: Colors.black,
-        shadowOffset: {
-            width: 2,
-            height: 6,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 8,
-        elevation: 3,
-    },
-    image: {
-        width: 200,
-        height: 200,
-    },
-    subtitle: {
-        ...Styles.textTitleSmall,
-        color: Colors.gray,
     },
     buttonContainer: {
         flexDirection: 'row',
