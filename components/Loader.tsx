60486d3aa434543c7c55f1de6fe7a35d844acda5
Isaac <fimbresisaac23@gmail.com>
2023-02-18 13:54:07 -0800

    new Loader component in the app

---
+++ components/Loader.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
+
+import { Colors } from '../constants'
+
+export const Loader: FC = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color={Colors.violet} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        zIndex: 5,
+        backgroundColor: Colors.black60,
+    }
+})
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 export * from "./AppButton";
+export * from "./Loader";
 export * from "./PointsBadge";
+export * from "./ProductsList";
 export * from "./SafeAreaContainer";
 export * from "./ScreenHeader";
-export * from "./ProductsList";
  
