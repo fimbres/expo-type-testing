a3a670606dca5df8132480ec3a8b9d49fc2ed664
Isaac <fimbresisaac23@gmail.com>
2023-02-19 11:50:16 -0800

    addding tests for formats functions

---
+++ utils/formats.test.js
@@ -0,0 +1,17 @@
+import { getDateLabel, getFormattedPoints } from "./formats";
+
+describe('Formats', () => {
+    it('format date correctly', () => {
+        const dateLabel = getDateLabel("2022-12-08T18:54:56.243Z");
+
+        expect(dateLabel).toBeDefined();
+        expect(dateLabel).toBe("4 de diciembre, 2022");
+    });
+
+    it('format points correctly', () => {
+        const pointsLabel = getFormattedPoints(10000);
+
+        expect(pointsLabel).toBeDefined();
+        expect(pointsLabel).toBe("10,000");
+    });
+});
