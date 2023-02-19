d753e24377e53a46417e5a1919a4825f99b195fb
Isaac <fimbresisaac23@gmail.com>
2023-02-19 13:45:36 -0800

    adding new test in navigation

---
+++ navigation/navigation.test.js
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { act, render, waitFor } from '@testing-library/react-native';
+
+import Navigation from './';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from '../slices/apiSlice';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper.js');
+jest.mock('expo-linking');
+
+describe('<Navigation />', () => {
+  test('navigation should render the HomeScreen first', async () => {
+    await act(async () => {
+      const { getByText } = await waitFor(() =>
+        render(
+          <ApiProvider api={apiSlice}>
+              <Navigation />
+          </ApiProvider>
+        )
+      );
+      const firstText = getByText('Bienvenido de vuelta!');
+      const secondText = getByText('Ruben Rodriguez');
+      const thirdText = getByText('Tus Puntos');
+      const fourthText = getByText('Tus Movimientos');
+  
+      expect(firstText).toBeDefined();
+      expect(secondText).toBeDefined();
+      expect(thirdText).toBeDefined();
+      expect(fourthText).toBeDefined();
+    });
+  });
+});
